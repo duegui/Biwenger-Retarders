01fcc93466ab953eafb1237272dc566d451d86eb
duegui <duegui@gmail.com>
2023-08-06 18:05:06 +0200

    test commit

--- st_biwenger.py
+++ st_biwenger.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
 
 st.title("Las noticias retarder")
 
@@ -12,9 +16,16 @@ jornada = 1
 
 st.header("Jornada LaLiga EA Sports: 1")
 
-from PIL import Image
+
 
 image = Image.open('foto.jpeg')
 
-st.subheader('Retarders de la jornada')
-st.image(image, caption='Two guys in the mountain')
+st.sidebar.subheader('Retarders de la jornada')
+st.sidebar.image(image, caption='Two guys in the mountain')
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['BuBufas Team', 'Riostra FC', 'Patoto FC'])
+
+st.area_chart(chart_data)
